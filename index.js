c85d31e15b6c79b0cb474f1c9a6c9798371f44b9
Lars <lars@recap.dk>
2025-03-27 17:15:51 +0100

    last_draft
    Think i completed the code!

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+function isValidPassword(password, username) {
+    if(password.length < 8) {
+        return false
+    }
+    if(password.includes(" ")) {
+        return false
+    }
+    if(password.includes(username)) {    
+        return false
+    }
+    else {
+        return true
+}
+}
+
